24a73afd4bf1d6d984710a7bf0808af3785422b4
Chris Browne <Chrismb2gun@gmail.com>
2021-08-24 00:40:57 +0200

    test

--- index.js
+++ index.js
@@ -474,35 +474,6 @@ function process_commands_query(query, mapKey, userid) {
     }
 }
 
-const say = require('say');
-
-function tts(voiceChannel, text) {
-    if (!FS.existsSync('./temp')) {
-        FS.mkdirSync('./temp');
-    }
-    const timestamp = new Date().getTime();
-    const soundPath = `./temp/${timestamp}.wav`;
-    say.export(text, null, 1, soundPath, (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        } else {
-            voiceChannel.join().then((connection) => {
-                connection.playFile(soundPath).on('end', () => {
-                    connection.disconnect();
-                    FS.unlinkSync(soundPath);
-                }).on('error', (err) => {
-                    console.error(err);
-                    connection.disconnect();
-                    FS.unlinkSync(soundPath);
-                });
-            }).catch((err) => {
-                console.error(err);
-            });
-        }
-    });
-}
-
 
 async function music_message(message, mapKey) {
     let replymsgs = [];
